f8cee4575ac5af53dbb398ca607ab11ae5a70713
edamame52525 <edamame19820810@gmail.com>
2025-01-29 21:11:34 +0900

    chatbot

--- src/tutorial/langchain_tutorial_v1.3.py
+++ src/tutorial/langchain_tutorial_v1.3.py
@@ -18,27 +18,7 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 os.environ["OPENAI_API_KEY"] = api_key
 
-# with open("long_text.txt", "w") as f:
-#     f.close() #ファイルに書き込むときはこれ使う
-
-
-
-"""
-long_text = 複数行リテラル
-print(len(long_text))
-text_list = text_splitter.split_text(long_text)
-# print(text_list)
-# print(len(text_list))
-
-
-# ドキュメント化
-document_list = text_splitter.create_documents([long_text])
-# print(document_list) #確認用
-# print(len(document_list)) #確認用
-
-
-↑のやり方では、テキストデータのリストを作成する。
-"""
+# スプリッターの設定
 text_splitter = CharacterTextSplitter(
     separator = "\n\n",
     chunk_size = 150,
@@ -49,28 +29,9 @@ text_splitter = CharacterTextSplitter(
 llm = ChatOpenAI(model_name = "gpt-4o-mini",temperature = 0,max_tokens=300)
 
 
-"""
-# ドキュメントの確認
-
-if not os.path.exists(file_path):
-    raise FileNotFoundError(f"ファイルが見つかりません: {file_path}")
-
-
-# ドキュメントを読み込む
-documents = loader.load()
-print(documents)
-"""
-
+#ローダー、エンベディングの設定
 file_path = './data.txt'
-
 loader = TextLoader(file_path, encoding='utf-8')
-
-
-
-
-
-
-# embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
 # ベクトルストアインデックスの作成
@@ -80,48 +41,46 @@ index = VectorstoreIndexCreator(
     text_splitter=text_splitter
 ).from_loaders([loader])
 
-"""
-.from_documents([document_list])では、データのリストそのものをindexに渡しているが、
-.from_loaders(loader)にすれば、いちいちドキュメントを分割せずに利用できるらしい。
-"""
+
+#タイトル
+st.title("中島家bot")
+
+user_question = st.text_input("質問",value="")
+
+if st.button("送信"):
+    if user_question.strip():
+
+        search_results = index.query(user_question,llm=llm)
+        template = """
+                    以下の情報に基づいて、文章を作成してください。箇条書きだけで大丈夫です。
+                    知識：{search_results}
+                    質問：{user_question}
+                    """
+        prompt = ChatPromptTemplate.from_messages([
+            ("system","あなたは猫で、語尾に「にゃん」とつけることができます"),
+            ("user",template)
+        ])
+
+        # 出力用インスタンス
+        output_parser = StrOutputParser()
+
+        # チェーンの作成
+        chain = prompt | llm | output_parser
+
+        #チェーンの実行
+        response = chain.invoke({
+            "search_results": search_results,
+            "user_question": user_question
+        })
+        
+        st.write(response)
 
 
 
 
 
 
-user_question = "まりんちゃんについて教えて"
-search_results = index.query(user_question,llm=llm)
-# print(search_results)
-# response =llm.generate(search_results)
-# print(response)
 
-
-template = """
-以下の情報に基づいて、文章を作成してください。分かりやすいように伝えてください。
-{search_results}
-
-質問：{user_question}
-"""
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system","あなたは猫で、語尾に「にゃん」とつけることができます"),
-    ("user",template)
-])
-
-# 出力用インスタンス
-output_parser = StrOutputParser()
-
-# チェーンの作成
-chain = prompt | llm | output_parser
-
-#チェーンの実行
-response = chain.invoke({
-    "search_results": search_results,
-    "user_question": user_question
-})
-
-print(response)
 
 
 
